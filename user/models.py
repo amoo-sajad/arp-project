75cd052ee2451f68d6fa750c54931350cb0a090b
sajad <dark.sjd00@gmail.com>
2022-11-02 10:01:13 -0400

    modify user

--- user/models.py
+++ user/models.py
@@ -7,7 +7,7 @@ from .managers import UserManager
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    phone_number = models.IntegerField(_('phone number'), unique=True)
+    phone_number = models.CharField(_('phone number'), unique=True, max_length=11)
     first_name = models.CharField(_('first name'), max_length=100)
     last_name = models.CharField(_('last name'), max_length=100)
     email = models.EmailField(_('email'), blank=True, null=True)
@@ -19,8 +19,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_expert = models.BooleanField(_('is expert'), default=False)
     is_staff = models.BooleanField(_('is staff'), default=False)
     wallet_balance = models.IntegerField(_('wallet balance'), default=0)
-    user_lat = models.DecimalField(_('user lat'), max_digits=9, decimal_places=6, blank=True, null=True)
-    user_long = models.DecimalField(_('user long'), max_digits=9, decimal_places=6, blank=True, null=True)
+    user_lat = models.DecimalField(_('user lat'), max_digits=9, decimal_places=6)
+    user_long = models.DecimalField(_('user long'), max_digits=9, decimal_places=6)
     created_at = models.DateTimeField(_('created at'), auto_now_add=True)
     updated_at = models.DateTimeField(_('updated at'), auto_now=True)
 
--- user/serializers.py
+++ user/serializers.py
@@ -10,7 +10,8 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = [
-            'phone_number', 'first_name', 'last_name', 'email', 'user_province', 'user_city', 'password'
+            'phone_number', 'first_name', 'last_name', 'email', 'user_province', 'user_city', 
+            'user_lat', 'user_long', 'password'
             ]
 
     def create(self, validated_data):
@@ -20,6 +21,8 @@ class UserSerializer(serializers.ModelSerializer):
             last_name=validated_data['last_name'],
             user_province=validated_data['user_province'],
             user_city=validated_data['user_city'],
+            user_lat=validated_data['user_lat'],
+            user_long=validated_data['user_long'],
             password=validated_data['password'],
             )
         user.email = validated_data['email']
