3153199c015864b29f252a868d27ab6e5f50da6a
sajad <dark.sjd00@gmail.com>
2022-11-12 08:06:32 -0500

    add admin api's to service app

--- service/urls.py
+++ service/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
-from .views import ServiceCreateAPIView, SkillCreateAPIView
+from .views import (
+    ServiceCreateAPIView, 
+    SkillCreateAPIView, 
+    ServiceListAPIView, 
+    SkillListAPIView,
+    ServiceDestroyAPIView,
+    SkillDestroyAPIView
+)
 
 urlpatterns = [
+    path('services/', ServiceListAPIView.as_view(), name='services'),
+    path('skills/', SkillListAPIView.as_view(), name='skills'),
     path('new-service/', ServiceCreateAPIView.as_view(), name='new-service'),
     path('new-skill/', SkillCreateAPIView.as_view(), name='new-skill'),
+    path('delete-service/', ServiceDestroyAPIView.as_view(), name='skills'),
+    path('delete-skill/', SkillDestroyAPIView.as_view(), name='skills'),
 ]
--- service/views.py
+++ service/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, AllowAny
 from .models import Service, Skill
 from .serializers import ServiceSerializer, SkillSerializer
 
@@ -17,3 +17,48 @@ class SkillCreateAPIView(generics.CreateAPIView):
     permission_classes = [IsAdminUser]
     authentication_classes = [JWTAuthentication]
     queryset = Skill.objects.all()
+
+
+class ServiceListAPIView(generics.ListAPIView):
+    serializer_class = ServiceSerializer
+    permission_classes = [AllowAny]
+    authentication_classes = [JWTAuthentication]
+    queryset = Service.objects.all()
+
+
+class SkillListAPIView(generics.ListAPIView):
+    serializer_class = SkillSerializer
+    permission_classes = [AllowAny]
+    authentication_classes = [JWTAuthentication]
+
+    def get_queryset(self):
+        service = self.request.GET.get('service', '')
+        return Skill.objects.filter(service=service)
+
+
+class ServiceDestroyAPIView(generics.DestroyAPIView):
+    serializer_class = ServiceSerializer
+    permission_classes = [IsAdminUser]
+    authentication_classes = [JWTAuthentication]
+    queryset = Service.objects.all()
+
+    def get_object(self):
+        service = self.request.GET.get('service', '')
+        queryset = self.get_queryset()
+        obj = get_object_or_404(queryset, title=service)
+        self.check_object_permissions(self.request, obj)
+        return obj
+
+
+class SkillDestroyAPIView(generics.DestroyAPIView):
+    serializer_class = SkillSerializer
+    permission_classes = [IsAdminUser]
+    authentication_classes = [JWTAuthentication]
+    queryset = Skill.objects.all()
+
+    def get_object(self):
+        skill = self.request.GET.get('skill', '')
+        queryset = self.get_queryset()
+        obj = get_object_or_404(queryset, title=skill)
+        self.check_object_permissions(self.request, obj)
+        return obj
